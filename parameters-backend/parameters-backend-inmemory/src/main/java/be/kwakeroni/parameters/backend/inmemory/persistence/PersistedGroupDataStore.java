c4bb5191715536332e5dfc1bcb68f697c222962e
kwakeroni <kwakeroni@gmail.com>
2018-03-19 07:35:14 +0100

    Provide initial entry when new group is created

--- parameters-backend/parameters-backend-inmemory/src/main/java/be/kwakeroni/parameters/backend/inmemory/persistence/PersistedGroupDataStore.java
+++ parameters-backend/parameters-backend-inmemory/src/main/java/be/kwakeroni/parameters/backend/inmemory/persistence/PersistedGroupDataStore.java
@@ -1,13 +1,11 @@
 package be.kwakeroni.parameters.backend.inmemory.persistence;
 
-import be.kwakeroni.evelyn.client.ClientTable;
 import be.kwakeroni.evelyn.client.DefaultClientTable;
 import be.kwakeroni.evelyn.model.DatabaseAccessor;
 import be.kwakeroni.evelyn.model.DatabaseException;
 import be.kwakeroni.evelyn.model.DatabaseProvider;
 import be.kwakeroni.evelyn.model.DefaultDatabaseProvider;
 import be.kwakeroni.evelyn.storage.StorageProvider;
-import be.kwakeroni.parameters.backend.inmemory.api.EntryData;
 import be.kwakeroni.parameters.backend.inmemory.api.GroupData;
 import be.kwakeroni.parameters.backend.inmemory.api.InMemoryGroup;
 import be.kwakeroni.parameters.backend.inmemory.service.GroupDataStore;
@@ -20,6 +18,7 @@ public class PersistedGroupDataStore implements GroupDataStore {
     public PersistedGroupDataStore(StorageProvider storageProvider) {
         this(storageProvider, DefaultDatabaseProvider.getInstance());
     }
+
     public PersistedGroupDataStore(StorageProvider storageProvider, DatabaseProvider provider) {
         this.storageProvider = storageProvider;
         this.provider = provider;
@@ -27,17 +26,32 @@ public class PersistedGroupDataStore implements GroupDataStore {
 
     @Override
     public GroupData getGroupData(InMemoryGroup group) {
-        return new PersistedGroupData(group, createTable(group));
-    }
-
-    private ClientTable<EntryData> createTable(InMemoryGroup group) {
-        DatabaseAccessor accessor;
         try {
-            accessor = this.provider.readCreate(storageProvider, group.getName());
-            return new DefaultClientTable<>(accessor, GroupTableOperation::valueOf);
+            return getGroupData0(group);
         } catch (DatabaseException exc) {
             throw new IllegalStateException(exc);
         }
+    }
+
+    private GroupData getGroupData0(InMemoryGroup group) throws DatabaseException {
+        GroupData data;
+        String databaseName = group.getName();
+
+
+        if (storageProvider.exists(databaseName)) {
+            DatabaseAccessor accessor = provider.read(storageProvider.read(databaseName));
+            data = getGroupData(group, accessor);
+        } else {
+            DatabaseAccessor accessor = provider.create(storageProvider, databaseName);
+            data = getGroupData(group, accessor);
+            group.initialData().forEach(data::addEntry);
+        }
+
+        return data;
+    }
+
+    private GroupData getGroupData(InMemoryGroup group, DatabaseAccessor accessor) {
+        return new PersistedGroupData(group, new DefaultClientTable<>(accessor, GroupTableOperation::valueOf));
     }
 
 }
--- parameters-backend/parameters-backend-inmemory/src/test/java/be/kwakeroni/parameters/backend/inmemory/persistence/PersistedGroupDataStoreTest.java
+++ parameters-backend/parameters-backend-inmemory/src/test/java/be/kwakeroni/parameters/backend/inmemory/persistence/PersistedGroupDataStoreTest.java
@@ -3,22 +3,29 @@ package be.kwakeroni.parameters.backend.inmemory.persistence;
 import be.kwakeroni.evelyn.model.DatabaseAccessor;
 import be.kwakeroni.evelyn.model.DatabaseException;
 import be.kwakeroni.evelyn.model.DatabaseProvider;
+import be.kwakeroni.evelyn.model.Event;
+import be.kwakeroni.evelyn.storage.Storage;
 import be.kwakeroni.evelyn.storage.StorageProvider;
 import be.kwakeroni.parameters.backend.inmemory.api.GroupData;
 import be.kwakeroni.parameters.backend.inmemory.api.InMemoryGroup;
+import be.kwakeroni.parameters.backend.inmemory.support.DefaultEntryData;
+import be.kwakeroni.test.factory.TestMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.extension.mockito.MockitoExtension;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static be.kwakeroni.evelyn.test.TestModel.event;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,7 +50,61 @@ class PersistedGroupDataStoreTest {
     void testCreateGroupData(@Mock DatabaseAccessor accessor) throws Exception {
 
         when(group.getName()).thenReturn("test.new.data");
-        when(databaseProvider.readCreate(any(), anyString())).thenReturn(accessor);
+        when(storageProvider.exists(any())).thenReturn(false);
+        when(databaseProvider.create(any(), anyString())).thenReturn(accessor);
+
+        GroupData groupData = dataStore.getGroupData(group);
+
+        assertThat(groupData).isInstanceOf(PersistedGroupData.class);
+        assertThat(groupData.getGroup()).isSameAs(group);
+        assertThat(groupData.getEntries()).hasSize(0);
+
+        verify(databaseProvider).create(storageProvider, "test.new.data");
+    }
+
+
+    @Test
+    @DisplayName("Adds initial entries to created group data")
+    void testAddInitialData(@Mock DatabaseAccessor accessor) throws Exception {
+
+        when(group.getName()).thenReturn("test.new.data");
+        when(storageProvider.exists(any())).thenReturn(false);
+        when(databaseProvider.create(any(), anyString())).thenReturn(accessor);
+        when(group.validateNewEntry(any(), any())).then(returnsFirstArg());
+        when(group.initialData()).thenReturn(Arrays.asList(
+                DefaultEntryData.of("id1", TestMap.of("param", "value1")),
+                DefaultEntryData.of("id2", TestMap.of("param", "value2"))
+        ));
+
+        GroupData groupData = dataStore.getGroupData(group);
+
+        assertThat(groupData).isInstanceOf(PersistedGroupData.class);
+        assertThat(groupData.getGroup()).isSameAs(group);
+
+        verify(databaseProvider).create(storageProvider, "test.new.data");
+        ArgumentCaptor<Event> events = ArgumentCaptor.forClass(Event.class);
+        verify(accessor, times(2)).append(events.capture());
+        Event event1 = events.getAllValues().get(0);
+        Event event2 = events.getAllValues().get(1);
+
+        assertThat(event1.getObjectId()).isEqualTo("id1");
+        assertThat(event1.getOperation()).isEqualTo("ADD");
+        assertThat(event1.getData()).isEqualTo("{\"param\":\"value1\"}");
+        assertThat(event2.getObjectId()).isEqualTo("id2");
+        assertThat(event2.getOperation()).isEqualTo("ADD");
+        assertThat(event2.getData()).isEqualTo("{\"param\":\"value2\"}");
+
+    }
+
+
+    @Test
+    @DisplayName("Reads persistent group data")
+    void testReadGroupData(@Mock Storage storage, @Mock DatabaseAccessor accessor) throws Exception {
+
+        when(group.getName()).thenReturn("test.new.data");
+        when(storageProvider.exists(any())).thenReturn(true);
+        when(storageProvider.read(any())).thenReturn(storage);
+        when(databaseProvider.read(any())).thenReturn(accessor);
         when(accessor.getData()).thenAnswer(__ -> Stream.of(
                 event("none", "1", "ADD", "{\"param\":\"value\"}", LocalDateTime.now()),
                 event("none", "2", "ADD", "{\"param\":\"value\"}", LocalDateTime.now()),
@@ -55,14 +116,18 @@ class PersistedGroupDataStoreTest {
         assertThat(groupData.getGroup()).isSameAs(group);
         assertThat(groupData.getEntries()).hasSize(2);
 
-        verify(databaseProvider).readCreate(storageProvider, "test.new.data");
+        verify(storageProvider).read("test.new.data");
+        verify(databaseProvider).read(storage);
     }
+
 
     @Test
     @DisplayName("Propagates Database Exception")
     void testCreateGroupDataException() throws Exception {
+        when(group.getName()).thenReturn("test.new.data");
+        when(storageProvider.exists(any())).thenReturn(false);
         DatabaseException exception = new DatabaseException("Test");
-        when(databaseProvider.readCreate(any(), any())).thenThrow(exception);
+        when(databaseProvider.create(any(), any())).thenThrow(exception);
 
         assertThatThrownBy(() -> dataStore.getGroupData(group))
                 .hasCause(exception);
